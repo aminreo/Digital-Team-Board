5f97743f35ccc354da086ce412846b1fada8d4c4
Amin <ayari.am@campus.lmu.de>
2025-12-31 21:56:54 +0100

    initial mock version

---
+++ Data/script.js
@@ -0,0 +1,5 @@
+const teamMembers = []
+
+teamMembers[0]= ("Joe Poe", "Likes to cycle daily, always helpful to others, always on time!")
+teamMembers[1]= ("Jessica Loe", "Enthusiastic guitar player, our company parties DJ!")
+teamMembers[2]= ("Eddy Voe", "IT Guy, fixes everyone tech crisis in matter of minutes!")
