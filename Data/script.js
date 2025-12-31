44da4e8bcece5f32f7343b811244b86434b81cab
Amin <ayari.am@campus.lmu.de>
2025-12-31 22:24:27 +0100

    Working prototype

--- Data/script.js
+++ Data/script.js
@@ -1,5 +1,19 @@
-const teamMembers = []
+const teamMembers = [];
 
-teamMembers[0]= ("Joe Poe", "Likes to cycle daily, always helpful to others, always on time!")
-teamMembers[1]= ("Jessica Loe", "Enthusiastic guitar player, our company parties DJ!")
-teamMembers[2]= ("Eddy Voe", "IT Guy, fixes everyone tech crisis in matter of minutes!")
+teamMembers[0] = { title: "Joe Poe", bio: "Likes to cycle daily, always helpful to others, always on time!" };
+teamMembers[1] = { title: "Jessica Loe", bio: "Enthusiastic guitar player, our company parties DJ!" };
+teamMembers[2] = { title: "Eddy Voe", bio: "IT Guy, fixes everyone tech crisis in matter of minutes!" };
+
+
+const tc = document.getElementById('team-container');
+// tc.style.color = "red";
+
+teamMembers.forEach(myFn);
+
+function myFn(item) {
+    tc.innerHTML +=
+       ` <div class="card">
+            <div class="title">${item.title}</div>
+            <div class="bio">${item.bio}</div>
+        </div>` ;
+}
