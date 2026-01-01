41d09cee5c00b89e999650bfc06491ea4df7f9af
Amin <ayari.am@campus.lmu.de>
2026-01-01 08:29:44 +0100

    working search function

--- Data/script.js
+++ Data/script.js
@@ -11,16 +11,35 @@ teamMembers.forEach(myFn);
 
 function myFn(item) {
     tc.innerHTML +=
-       ` <div class="card">
+        ` <div class="card">
             <div class="title">${item.title}</div>
             <div class="bio">${item.bio}</div>
         </div>` ;
 }
 
-const sb=document.getElementById('search-bar').addEventListener('input',(e)=>sbFn(e))
+const sb = document.getElementById('search-bar').addEventListener('input', (e) => sbFn(e))
 
-function sbFn(e){
+function sbFn(e) {
     console.log('ok')
     const term = e.target.value.toLowerCase();
+    tc.innerHTML = '';
+    if (term == '') {
+        teamMembers.forEach(myFn);
+        return;
+    }
     // todo
+    for (item in teamMembers) {
+        const current_card = teamMembers[item]
+
+        if (current_card.title.toLowerCase().indexOf(term) > -1 ||
+            current_card.bio.toLowerCase().indexOf(term) > -1) {
+            tc.innerHTML +=
+                ` <div class="card">
+            <div class="title">${current_card.title}</div>
+            <div class="bio">${current_card.bio}</div>
+        </div>` ;
+        } else {
+
+        }
+    }
 }
