37a46525b8f8a29e2581456dc071a50bb53c7d87
Amin <ayari.am@campus.lmu.de>
2026-01-01 09:40:37 +0100

    added team members images

--- Data/script.js
+++ Data/script.js
@@ -1,8 +1,8 @@
 const teamMembers = [];
 
-teamMembers[0] = { title: "Joe Poe", bio: "Likes to cycle daily, always helpful to others, always on time!" };
-teamMembers[1] = { title: "Jessica Loe", bio: "Enthusiastic guitar player, our company parties DJ!" };
-teamMembers[2] = { title: "Eddy Voe", bio: "IT Guy, fixes everyone tech crisis in matter of minutes!" };
+teamMembers[0] = { title: "Joe Poe", bio: "Likes to cycle daily, always helpful to others, always on time!", img: "joe.jpg" };
+teamMembers[1] = { title: "Jessica Loe", bio: "Enthusiastic guitar player, our company parties DJ!", img: "jessica.jpg" };
+teamMembers[2] = { title: "Eddy Voe", bio: "IT Guy, fixes everyone tech crisis in matter of minutes!", img: "eddy.jpg" };
 
 
 const tc = document.getElementById('team-container');
@@ -12,6 +12,7 @@ teamMembers.forEach(addCardHTML);
 function addCardHTML(item) {
     tc.innerHTML +=
         ` <div class="card">
+            <img src="Data/Img/${item.img}" width="150" height="100"> 
             <div class="title">${item.title}</div>
             <div class="bio">${item.bio}</div>
         </div>` ;
@@ -27,16 +28,18 @@ function sbFn(e) {
         teamMembers.forEach(addCardHTML);
         return;
     }
-    // todo
-    for (item in teamMembers) {
-        const current_card = teamMembers[item]
+    //filter version
+    const filtredTeam = teamMembers.filter((item) => handleItem(item, term));
+    if (filtredTeam.length == 0) {
+        tc.innerHTML = `<div class="not-found-search">No team members found...<br>maybe you're the first one with that hobby? :)</div>`;
+    } else {
+        filtredTeam.forEach(addCardHTML)
+    }
+}
 
-        if (current_card.title.toLowerCase().indexOf(term) > -1 ||
-            current_card.bio.toLowerCase().indexOf(term) > -1) {
-            addCardHTML(current_card);
-        } 
-    }
-    if (tc.innerHTML == ''){
-        tc.innerHTML+= `<div style="font-style: italic;padding: 10px;">No team members found...<br>maybe you're the first one with that hobby? :)</div>`;
-    }
+
+function handleItem(item, term) {
+    const res = item.title.toLowerCase().indexOf(term) > -1 ||
+        item.bio.toLowerCase().indexOf(term) > -1;
+    return res;
 }
